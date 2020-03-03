0747e52050283ac279cd73c73af6524523db0715
william175 <bill5986@gmail.com>
2020-03-03 17:57:41 +0800

    add index.js

---
+++ src/content/js/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector('#popoBtn').addEventListener('click', function () {
+    document.querySelector('#popobox').classList.toggle('open');
+});
+
+document.querySelector('#close').addEventListener('click', function () {
+    document.querySelector('#popobox').classList.toggle('open');
+});
